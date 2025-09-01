4130043527d50a13a0ba8c7b3b4429f5f444af86
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-09-01 15:36:49 +0700

    add login and register page

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { SignIn } from "@clerk/nextjs";
+
+export default function LoginPage() {
+  return (
+    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+      <Image src="/assets/logo.svg" alt="Logo" width={100} height={100} />
+
+      <div className="mt-3">
+        <SignIn />
+      </div>
+    </main>
+  );
+}
